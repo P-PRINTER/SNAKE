fd5e3dfe5936cebcc5f1d9330ccec96fa110781e
P-PRINTER <psevdoprinter@gmail.com>
2023-10-27 02:51:34 +0200

    the "gameArea" parameter of the "snakeCanvas" function at the snake-game__canvas.js was renamed to "gameBlock"

--- common.blocks/snake-game/__canvas/snake-game__canvas.js
+++ common.blocks/snake-game/__canvas/snake-game__canvas.js
@@ -2,13 +2,13 @@ import mainLayer from "./_layer/snake-game__canvas_layer_main.js";
 import backLayer from "./_layer/snake-game__canvas_layer_back.js";
 
 
-export default function snakeCanvas (gameArea, container, {
+export default function snakeCanvas (gameBlock, container, {
 	widthInCells = 15,
 	heightInCells = 15,
 	cellSize = 10,
 }) {
 
-	const canvas = gameArea.querySelectorAll(".snake-game__canvas");
+	const canvas = gameBlock.querySelectorAll(".snake-game__canvas");
 
 	for (let canvas of canvases) {
 
