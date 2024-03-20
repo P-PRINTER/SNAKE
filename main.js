3e0e2d65d603da6093ea2d9464b0dfa974732a4c
P-PRINTER <psevdoprinter@gmail.com>
2024-03-21 00:01:41 +0100

    update main.js

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
 import gameBlock from "./common.blocks/game-block/game-block.js"
 
-
-let codeBlocks = [
-	gameBlock,
-];
-
-runBlocks(codeBlocks);
-
-
-function runBlocks(blockArr) {
-	for (let block of blockArr) {
-		block.run();
-	}
-}
+gameBlock.run();
