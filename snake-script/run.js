e95369538fedabc8516225056146a53502690177
P-PRINTER <psevdoprinter@gmail.com>
2024-03-22 00:17:12 +0100

    update Main class

--- snake-script/run.js
+++ snake-script/run.js
@@ -12,6 +12,7 @@ export class Main {
     static init (snake) {
         if ( !(snake instanceof Snake) ) throw Error('"snake" arg of Main-obj init must have Snake type.');
 
+        snake.setActor( Game.getActor() );
         this._snake = snake;
         Controller.init();
         Game.init();
@@ -22,6 +23,7 @@ export class Main {
         if ( !(snake instanceof Snake) ) throw Error('"snake" arg of Main-obj init must have Snake type.');
 
         Game.describe(this._snake);
+        snake.setActor( this._snake.getActor() );
         this._snake = snake;
         Game.subscribe(this._snake);
     }
