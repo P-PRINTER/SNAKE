5b407acbc075cee9dd91dc6ae14c1587a52c857f
P-PRINTER <psevdoprinter@gmail.com>
2024-03-22 00:09:37 +0100

    create run.js and code static Main class at there

---
+++ snake-script/run.js
@@ -0,0 +1,28 @@
+import { Game } from "./game";
+import { Snake } from "./snake";
+
+export class Main {
+
+    constructor () {
+        throw Error('"new Main()" is an invalid construction, Main is a static class.');
+    }
+
+    static _snake = null;
+
+    static init (snake) {
+        if ( !(snake instanceof Snake) ) throw Error('"snake" arg of Main-obj init must have Snake type.');
+
+        this._snake = snake;
+        Controller.init();
+        Game.init();
+        Game.subscribe(snake);
+    }
+
+    static replaceSnake (snake) {
+        if ( !(snake instanceof Snake) ) throw Error('"snake" arg of Main-obj init must have Snake type.');
+
+        Game.describe(this._snake);
+        this._snake = snake;
+        Game.subscribe(this._snake);
+    }
+}
